339d3b7ac279593137ca6d490eddba33e9892e0b
ishanguptaMonk <ishan.gupta182002@gmail.com>
2024-10-02 22:51:05 +0530

    commit all file

---
+++ src/components/footer/FooterAdmin.tsx
@@ -0,0 +1,31 @@
+'use client';
+/*eslint-disable*/
+
+import {
+  Flex,
+  List,
+  ListItem,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import Link from '@/components/link/Link';
+import { SearchBar } from '../search';
+
+export default function Footer() {
+  const textColor = useColorModeValue('gray.500', 'white');
+  return (
+    <Flex
+      zIndex="3"
+      flexDirection={{
+        base: 'column',
+        xl: 'row',
+      }}
+      alignItems="center"
+      justifyContent="space-between"
+      px={{ base: '30px', md: '50px' }}
+      pb="30px"
+    >
+      <SearchBar me="10px" borderRadius="30px" />
+    </Flex>
+  );
+}
